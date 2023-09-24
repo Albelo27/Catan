ef3b3c45b7f39b97df5b576b376ae24dcf244fc4
CompiledError <70342656+CompiledError@users.noreply.github.com>
2023-09-24 16:51:44 -0700

    testCommit

--- app/src/main/java/com/example/catan/MainActivity.java
+++ app/src/main/java/com/example/catan/MainActivity.java
@@ -11,4 +11,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    //I am trying to see how to push changes from my machine to the github after initial commit
 }
